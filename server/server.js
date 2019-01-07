8ff5792df33096e75c0f56e52c25dd73f67dd9b1
Patrick Hogan <phogan110@live.com>
2019-01-07 11:16:26 -0700

    imported path and fs; set up express.static

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const apiRouter = require("./routes");
+const path = require("path");
+const fs = require("fs");
 
 let app = express(); //call express with app
 
@@ -8,5 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
+
+app.use(express.static(path.join(__dirname, "../client")));
 
 app.listen(3000);
