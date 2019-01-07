68fb8afee7326a256cf35da2419b437fcb60c13d
Patrick Hogan <phogan110@live.com>
2019-01-07 15:58:57 -0700

    created `displayChirps`; updated GET && POST requests

--- client/app.js
+++ client/app.js
@@ -3,21 +3,28 @@ $(() => {
   const $user = $("#user");
   const $chirpText = $("#chirp-text");
 
+  displayChirps = chirps => {
+    $chirps.empty();
+    $.each(chirps, (i, chirp) => {
+      if (chirp.user && chirp.text !== undefined) {
+        $chirps.append(
+          '<div class="card w-25 text-center m-2 rounded"><div class="card-body"><h4 class="card-title">' +
+            chirp.user +
+            '</h4><p class="card-text">' +
+            chirp.text +
+            "</p>" +
+            `<button class="btn btn-danger" onclick="removeChirp(${i})">X</button>` +
+            "</div></div>"
+        );
+      }
+    });
+  }; //update chirps render
+
   $.ajax({
     type: "GET",
     url: "api/chirps",
     success: chirps => {
-      $.each(chirps, (i, chirp) => {
-        if (chirp.user && chirp.text !== undefined) {
-          $chirps.append(
-            '<div class="card w-25 text-center m-2 rounded"><div class="card-body"><h4 class="card-title">' +
-              chirp.user +
-              '</h4><p class="card-text">' +
-              chirp.text +
-              "</p></div></div>"
-          );
-        }
-      });
+      displayChirps(chirps);
     },
     error: err => {
       console.log(err);
@@ -28,11 +35,17 @@ $(() => {
     let chirp = {
       user: $user.val(),
       text: $chirpText.val()
-    }; //create chirp object with the input values from name and chirpText
+    }; //create chirp object with the input values from user and chirpText
     $.ajax({
       type: "POST",
       url: "api/chirps",
-      data: chirp
-    });
+      data: chirp,
+      success: chirps => {
+        displayChirps(chirps);
+      },
+      error: err => {
+        console.log(err);
+      }
+    }); //update chirps.json with new chirp; if any errors arise log them in console
   });
 });
