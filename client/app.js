a917abee584d2eba463e04dd34fd10e880fbea71
Patrick Hogan <phogan110@live.com>
2019-01-07 14:24:12 -0700

    modified success call with if statement to prevent undefined

--- client/app.js
+++ client/app.js
@@ -6,13 +6,15 @@ $(() => {
     url: "api/chirps",
     success: chirps => {
       $.each(chirps, (i, chirp) => {
-        $chirps.append(
-          '<div class="card w-25 text-center m-2 rounded"><div class="card-body"><h4 class="card-title">' +
-            chirp.user +
-            '</h4><p class="card-text">' +
-            chirp.text +
-            "</p></div></div>"
-        );
+        if (chirp.user && chirp.text !== undefined) {
+          $chirps.append(
+            '<div class="card w-25 text-center m-2 rounded"><div class="card-body"><h4 class="card-title">' +
+              chirp.user +
+              '</h4><p class="card-text">' +
+              chirp.text +
+              "</p></div></div>"
+          );
+        }
       });
     }
   });
