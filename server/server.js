2693f7a8dead0a51186360380262df803cc59c90
Patrick Hogan <phogan110@live.com>
2019-01-07 11:45:00 -0700

    removed useless import

--- server/server.js
+++ server/server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const apiRouter = require("./routes");
 const path = require("path");
-const fs = require("fs");
 
 let app = express(); //call express with app
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", apiRouter);
 
